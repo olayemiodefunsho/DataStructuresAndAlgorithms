63119acb7faf018afbfe5de11ce449844f601df6
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-11 23:50:26 +0100

    remove

--- linkedListConstruction.py
+++ linkedListConstruction.py
@@ -22,10 +22,23 @@ class DoublyLinkedList:
         pass
 
     def remove(self, node):
-        pass
+        if (node == self.head):
+            self.head = self.head.next
+        if (node == self.tail):
+            self.tail = self.tail.prev
+        self.removeNodeBindings(node)
 
     def containsNodesWithValue(self, value):
         node = self.head
         while node is not None and node.value != value:
             node = node.next
         return node is not None
+
+    def removeNodeBindings(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.prev = None
+        node.next = None
+        
