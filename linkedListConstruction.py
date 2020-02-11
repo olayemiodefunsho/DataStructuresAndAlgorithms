6f4339265347ae411b8a91c67b76880e22813f4b
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-11 23:37:21 +0100

    prototype

---
+++ linkedListConstruction.py
@@ -0,0 +1,28 @@
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        pass
+
+    def setTail(self, node):
+        pass
+
+    def insertBefore(self, node, nodeToInsert):
+        pass
+
+    def insertAfter(self, node, nodeToInsert):
+        pass
+
+    def insertAtPosition(self, node, nodeToInsert):
+        pass
+
+    def removeNodesWithValue(self, node):
+        pass
+
+    def remove(self, node):
+        pass
+
+    def containsNodesWithValue(self, node):
+        pass
