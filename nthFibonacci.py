eefddd3acd94312fa762e4a42dcd7a028c7a3aa3
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-13 22:16:37 +0100

    memoize

--- nthFibonacci.py
+++ nthFibonacci.py
@@ -1,8 +1,16 @@
-#O(2^n) time | O(n) space
-def getNthFib(n):
-    if n == 2:
-        return 1
-    elif n == 1:
-        return 0
+# O(2^n) time | O(n) space
+# def getNthFib(n):
+#     if n == 2:
+#         return 1
+#     elif n == 1:
+#         return 0
+#     else:
+#         return getNthFib(n - 1) + getNthFib(n - 2)
+
+# O(n) time | O(n) space
+def getNthFib(n, memoize = {1 : 0, 2 : 1}):
+    if n in memoize:
+        return memoize[n]
     else:
-        return getNthFib(n - 1) + getNthFib(n - 2)
+        memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize)
+        return memoize[n]
