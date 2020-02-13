61b10684b93f65a9c5251e7bd3e576a57f1badc4
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-13 22:25:00 +0100

    interation

--- nthFibonacci.py
+++ nthFibonacci.py
@@ -8,9 +8,20 @@
 #         return getNthFib(n - 1) + getNthFib(n - 2)
 
 # O(n) time | O(n) space
-def getNthFib(n, memoize = {1 : 0, 2 : 1}):
-    if n in memoize:
-        return memoize[n]
-    else:
-        memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize)
-        return memoize[n]
+# def getNthFib(n, memoize = {1 : 0, 2 : 1}):
+#     if n in memoize:
+#         return memoize[n]
+#     else:
+#         memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize)
+#         return memoize[n]
+
+# O(n) time | O(1) space
+def getNthFib(n):
+    lastTwo = [0, 1]
+    counter = 3
+    while counter <= n:
+        nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    return lastTwo[1] if n > 1 else lastTwo[0]
