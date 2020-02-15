1cccd3ca5f453b248eb5d072656099f4221f6e42
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-15 23:01:20 +0100

    bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+#O(n^2) time | O(1)
+def bubblesort(array):
+    isSorted = False
+    counter = 0
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) - 1 - counter):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                isSorted = False
+        counter += 1
+    return array
+
+    
