13dbba4a2723b1fa44196e1040f76cbb2494bbec
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-18 23:27:58 +0100

    pointer

--- palindrome.py
+++ palindrome.py
@@ -18,10 +18,21 @@
 #     return True if i >= j else string[i] == string[j] and isPalindrome(string, i + 1)
 
 # O(n) time | O(n) space
-def isPalindrome(string, i = 0):
-    j = len(string) - 1 - i
-    if i >= j:
-        return True
-    if string[i] != string[j]:
-        return False
-    return isPalindrome(string, i + 1)
+# def isPalindrome(string, i = 0):
+#     j = len(string) - 1 - i
+#     if i >= j:
+#         return True
+#     if string[i] != string[j]:
+#         return False
+#     return isPalindrome(string, i + 1)
+
+# O(n) time | O(1) space
+def isPalindrome(string):
+    leftIdx = 0
+    rightIdx = len(string) - 1
+    while leftIdx < rightIdx:
+        if string[leftIdx] != string[rightIdx]:
+            return False
+        leftIdx += 1
+        rightIdx -= 1
+    return True
