349582df410edd77417feae74627b4ebc3e63134
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-18 23:23:43 +0100

    tail recursion

--- palindrome.py
+++ palindrome.py
@@ -13,7 +13,15 @@
 #     return string == "".join(string[i])
 
 # O(n) time | O(n) space
+# def isPalindrome(string, i = 0):
+#     j = len(string) - 1 - i
+#     return True if i >= j else string[i] == string[j] and isPalindrome(string, i + 1)
+
+# O(n) time | O(n) space
 def isPalindrome(string, i = 0):
     j = len(string) - 1 - i
-    return True if i >= j else string[i] == string[j] and isPalindrome(string, i + 1)
-     
+    if i >= j:
+        return True
+    if string[i] != string[j]:
+        return False
+    return isPalindrome(string, i + 1)
