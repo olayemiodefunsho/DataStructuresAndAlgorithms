b7be699478adb49d43eaf7f3916c9402cb5b2f12
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-23 22:43:09 +0100

    bst construction insert

---
+++ bstConstruction.py
@@ -0,0 +1,24 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    #Average: O(log(n)) time | O(1) space
+    #Worst: O(n) time | O(1) space
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
