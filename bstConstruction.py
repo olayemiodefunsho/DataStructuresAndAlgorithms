52196313983abba1f65f2bef2a25cd9a931dd3e6
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-02-25 22:19:13 +0100

    bst construction remove II

--- bstConstruction.py
+++ bstConstruction.py
@@ -46,4 +46,25 @@ class BST:
                 parentNode = currentNode
                 currentNode = currentNode.right
             else:
-                pass
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(currentNode.value, currentNode)
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.right = currentNode.right.right
+                    else:
+                        currentNode.value = None
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+                break
+        return self
+
+    def getMinValue(self):
+        currentNode = self
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
